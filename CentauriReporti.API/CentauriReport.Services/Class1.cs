75dfcf2e6a610bf73a6320fd766192a270f38c63
Marc Kenneth Capitulo-Lomio <mlomio@blastasia.com>
2021-12-16 17:26:41 +0800

    ..

---
+++ CentauriReporti.API/CentauriReport.Services/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace CentauriReport.Services
+{
+    public class Class1
+    {
+    }
+}
---
+++ CentauriReporti.API/CentauriReport.Shared/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace CentauriReport.Shared
+{
+    public class Class1
+    {
+    }
+}
